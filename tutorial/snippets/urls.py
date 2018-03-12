63b743649c4453d8c20d1b1056c41dbaa13c7932
Titov Anton <webdev@titovanton.com>
2018-03-12 05:33:31 +0000

    Adding optional format suffixes to our URLs

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
+
 from snippets import views
 
 
@@ -6,3 +8,5 @@ urlpatterns = [
     url(r'^snippets/$', views.snippet_list),
     url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
