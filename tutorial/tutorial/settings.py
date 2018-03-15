84834372e76a9411bbbdc1d6171845e8f93d2f7e
Titov Anton <webdev@titovanton.com>
2018-03-15 16:05:05 +0000

    Adding pagination

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -122,3 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Rest Framework
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
