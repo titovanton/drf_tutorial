ef6eb2a1a663f41e86a058daba3266344e4186a0
Titov Anton <webdev@titovanton.com>
2018-03-15 09:03:04 +0000

    Creating an endpoint for the highlighted snippets

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
     url(r'^users/$', views.UserList.as_view(), name='user-list'),
     url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
+    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', views.SnippetHighlight.as_view()),
     url(r'^$', views.api_root),
 ]
 
--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -4,6 +4,7 @@ from rest_framework import permissions
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
+from rest_framework import renderers
 
 from .models import Snippet
 from .serializers import SnippetSerializer, UserSerializer
@@ -42,3 +43,12 @@ def api_root(request, format=None):
         'users': reverse('user-list', request=request, format=format),
         'snippets': reverse('snippet-list', request=request, format=format)
     })
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = (renderers.StaticHTMLRenderer,)
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
