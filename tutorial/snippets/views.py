9a75807791085a239a44cc69c35b4621448739c2
Titov Anton <webdev@titovanton.com>
2018-03-15 16:52:54 +0000

    Using Routers

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,8 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import permissions
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.reverse import reverse
 from rest_framework import renderers
 from rest_framework import viewsets
 from rest_framework.decorators import detail_route
@@ -39,11 +37,3 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
-
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'snippets': reverse('snippet-list', request=request, format=format)
-    })
