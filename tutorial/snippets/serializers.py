3209cec2c09f3d5bcd490f368f60dc90c3004a5a
Titov Anton <webdev@titovanton.com>
2018-03-15 09:45:52 +0000

    Hyperlinking owner field

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -5,14 +5,15 @@ from snippets.models import Snippet
 
 
 class SnippetSerializer(serializers.HyperlinkedModelSerializer):
-    owner = serializers.ReadOnlyField(source='owner.username')
+    owner = serializers.HyperlinkedRelatedField(
+        view_name='user-detail', read_only=True)
     highlight = serializers.HyperlinkedIdentityField(
         view_name='snippet-highlight', format='html')
 
     class Meta:
         model = Snippet
-        fields = ('url', 'id', 'highlight', 'owner', 'title', 'code', 'linenos',
-                  'language', 'style')
+        fields = ('url', 'id', 'highlight', 'owner', 'title',
+                  'code', 'linenos', 'language', 'style')
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -5,6 +5,7 @@ from snippets import views
 
 
 urlpatterns = [
+    url(r'^$', views.api_root),
     url(r'^snippets/$', views.SnippetList.as_view(),
         name='snippet-list'),
     url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view(),
@@ -16,7 +17,6 @@ urlpatterns = [
         name='user-list'),
     url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view(),
         name='user-detail'),
-    url(r'^$', views.api_root),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
